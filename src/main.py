ab3d2c2a758f8d6940cb97aff8388719d521ac27
Maria Nicole Bulalacao <B00157129@mytudublin.ie>
2026-02-16 20:02:34 +0000

    It works nowand loops through every image but now I dont know what image is being presented so this time im gonna make it it says what image it is at the top

--- src/main.py
+++ src/main.py
@@ -1,32 +1,36 @@
 import cv2
 import numpy as np
 import time
-
+import os
 
 def main():
     # Path to the image you want to load
-    img_path = "data/images"
+    img_folder = "C:/Users/Bulal/PyCharmMiscProject/CV-OringInspection/data/images"
 
-    # Read the image and loads the image aas a 3 channel BGR image
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    # Loop through every image in the folder
+    for filename in os.listdir(img_folder):
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(img_folder, filename)
+            # Read the image and loads the image aas a 3 channel BGR image
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
-    # if the image is not found or failed to load,
-    # the image will be null.
-    if img is None:
-        raise FileNotFoundError(f"Coould not read image")
+            # if the image is not found or failed to load,
+            # the image will be null.
+            if img is None:
+                raise FileNotFoundError(f"Coould not read image")
 
-    # Convert the image to a greyscale
-    # greyscalling is easier for thresholding and histogram
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Convert the image to a greyscale
+            # greyscalling is easier for thresholding and histogram
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Display the grayscale image in a window titled "gray"
-    cv2.imshow("Gray", gray)
+            # Display the grayscale image in a window titled "gray"
+            cv2.imshow("Gray", gray)
 
-    # wait until a key is pressed
-    cv2.waitKey(0)
+            # wait until a key is pressed
+            cv2.waitKey(0)
 
-    # close all opencv windows
-    cv2.destroyAllWindows()
+        # close all opencv windows
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
